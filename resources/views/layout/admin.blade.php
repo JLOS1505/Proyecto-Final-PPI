d5ef988608400e51ff459a9455082b4e7a1ee2fc
unknown <jlos150502@gmail.com>
2024-05-03 19:02:56 -0600

    Fix

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -75,8 +75,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Merk-Ditto</span>
     </a>
 
     <!-- Sidebar -->
@@ -245,7 +244,7 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2014-2021 <a>MERK-DITTO</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.1.0
