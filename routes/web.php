f62f4ab30cdb520474defe60263249bb546eaffa
unknown <jlos150502@gmail.com>
2024-05-04 20:45:08 -0600

    Ruta de VentaController agregada a Routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProveedorController;
 use App\Http\Controllers\IngresoController;
+use App\Http\Controllers\VentaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,6 @@ Route::resource('almacen/producto', ProductoController::class);
 Route::resource('ventas/clientes', ClienteController::class);
 Route::resource('compras/proveedor', ProveedorController::class);
 Route::resource('compras/ingreso', IngresoController::class);
+Route::resource('ventas/venta', VentaController::class);
 
 
