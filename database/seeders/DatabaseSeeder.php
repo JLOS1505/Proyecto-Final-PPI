b7112f0464350c83ef64a118e34c3523daf1c6f3
unknown <jlos150502@gmail.com>
2024-05-08 18:41:02 -0600

    Seeder Tabla Producto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Producto;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $producto = new Producto();
+
+        $producto->id_categoria = "1";
+        $producto->codigo = "1";
+        $producto->nombre = "Seeder Funcional";
+        $producto->stock = "10";
+        $producto->descripcion = "seeder";
+        $producto->imagen = "";
+        $producto-> estado = "Activo";
+
+        $producto->save();
     }
 }
